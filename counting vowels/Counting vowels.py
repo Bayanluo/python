f341b320dba1851f0f02562d9713aba3554bd84a
Bayanluo <nargessbayanloo@gmail.com>
2024-09-18 15:05:40 +0330

    Add files via upload

---
+++ counting vowels/Counting vowels.py
@@ -0,0 +1,16 @@
+a=input('enter a word:')
+c=0
+for i in a:
+    if i=='a':
+        c+=1
+    elif i=='e':
+        c+=1
+    elif i=='i':
+        c+=1
+    elif i=='o':
+        c+=1
+    elif i=='u':
+        c+=1
+    else:
+        continue
+print(c)
